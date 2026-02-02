2054e9561116b52137b7fff97d6ffdb36c3e9610
vijayvaddi11 <vijayjay6688@gmail.com>
2026-02-02 18:11:46 +0530

    add register mongoose schema

---
+++ models/registerSchema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const userRegisterSchema = new Schema({
+     username:{
+          type:String,
+          required: true,
+          unique: true
+     },
+     email:{
+          type:String,
+          required:true,
+          unique:true
+     },
+     password:{
+          type:String,
+          required:true
+     }
+},{timestamps:true})
+
+export default mongoose.model('User',userRegisterSchema)
